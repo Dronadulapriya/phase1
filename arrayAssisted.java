5efb56568e6f8329de5aae455c64abb7303b0763
Dronadulapriya <100116825+Dronadulapriya@users.noreply.github.com>
2022-03-02 10:23:43 +0530

    Add files via upload

---
+++ arrayAssisted.java
@@ -0,0 +1,22 @@
+public class arrayAssisted
+{
+
+public static void main(String[] args) 
+{
+
+int a[]= {1,2,3,4,5};
+for(int i=0;i<5;i++) 
+{
+System.out.println("Elements of array a: "+a[i]);
+}
+
+int[][] b = {
+            {1, 4, 8, 6}, 
+            {9, 6, 2} };
+      
+      System.out.println("\nLength of row 1: " + b[0].length);
+      }
+}
+
+
+
