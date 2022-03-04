bf4e09f23e8c8cf51df9af244eda9594b0c6b9e5
Dronadulapriya <100116825+Dronadulapriya@users.noreply.github.com>
2022-03-04 13:21:03 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,25 +1,34 @@
 package com.phase1.priya;
 
-
-class MyException extends Exception 
+class RotateArray 
 { 
-    public MyException(String s) 
-    { 
-        super(s); 
-    } 
-} 
-public class Main 
-{ 
-    public static void main(String args[]) 
-    { 
-        try
-        { 
-            throw new MyException("temp"); 
-        } 
-        catch (MyException ex) 
-        { 
-            System.out.println("Caught"); 
-            System.out.println(ex.getMessage()); 
-        } 
-    } 
+public void rotate(int[] nums, int k) 
+{
+    		if(k > nums.length) 
+       			k=k%nums.length;
+ 		int[] result = new int[nums.length];
+ 		for(int i=0; i < k; i++){
+        			result[i] = nums[nums.length-k+i];
+ 		}
+ 		int j=0;
+    		for(int i=k; i<nums.length; i++)
+    		{
+        			result[i] = nums[j];
+j++;
+    		}
+ 		System.arraycopy( result, 0, nums, 0, nums.length );
 }
+} 
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		RotateArray r = new RotateArray();
+        		int arr[] = { 1, 2, 3, 4, 5, 6, 7 }; 
+        		r.rotate(arr, 5); 
+        		for(int i=0;i<arr.length;i++){
+            			System.out.print(arr[i]+" ");
+        		}
+	}
+}
+
